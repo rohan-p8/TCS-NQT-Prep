101a9e1a10eb7a660dec070320668d45ef7682af
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-07 23:42:27 +0530

    First problem of NQT prep

--- Problem1.py
+++ Problem1.py
@@ -1,1 +1,15 @@
-# First problem
+"""
+Problem Statement
+
+Write a program that checks whether a given number is:
+
+Even, or
+Odd
+"""
+
+a = int(input("Enter a number: "))
+
+if a % 2 == 0:
+    print(f"{a} is an Even number.")
+else:
+    print(f"{a} is an Odd number.")
