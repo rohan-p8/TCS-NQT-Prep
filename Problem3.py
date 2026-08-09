74d14d7f5e21e4521dd8ce7279f17e6beb92d80d
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-09 23:40:21 +0530

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -6,3 +6,12 @@ Question 3: Factorial of a Number
 The factorial of a number n means multiplying all positive integers 
 from 1 to n.
 """
+
+n = int(input("Enter a number: "))
+
+fact =1
+
+for i in range(1, n + 1):
+    fact = fact * i
+
+print(f"The factorial of {n} is: {fact}")
