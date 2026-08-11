e71171c81e45b359debe763e899acf35c834605d
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-11 23:07:46 +0530

    Fibonacci series

--- Problem4.py
+++ Problem4.py
@@ -6,3 +6,15 @@ In the Fibonacci series, every new number is the sum of the
 previous two numbers.
 
 """
+n = int(input("Enter the number of terms: "))
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=' ')
+    c = a + b
+    a = b
+    b = c
+    
+
