b967dd97904cbafac505f008e8daee64fb92d411
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-13 22:48:20 +0530

    Reverse a given no.

--- Problem5.py
+++ Problem5.py
@@ -1,3 +1,18 @@
 """
 Write a program to reverse the digits of a given number.
 """
+"""
+
+"""
+
+n = int(input("Enter a number: "))
+
+reverse = 0
+
+while n > 0:
+    last_digit = n % 10
+    reverse = (reverse * 10) + last_digit
+    n = n // 10
+
+
+print("Reversed number:", reverse)
