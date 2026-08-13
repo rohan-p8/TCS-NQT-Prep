53de3e45dcba30c3abce1205494d32092a43a9ff
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-13 22:51:12 +0530

    Reverse a given no.

--- Problem5.py
+++ Problem5.py
@@ -2,7 +2,14 @@
 Write a program to reverse the digits of a given number.
 """
 """
+2. Logic
 
+The idea is simple:
+
+-Take the last digit of the number.
+-Add it to a new number.
+-Remove the last digit from the original number.
+-Repeat until the original number becomes 0.
 """
 
 n = int(input("Enter a number: "))
